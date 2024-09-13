c7b9cef0dbbae541e5fbd8fe17271e8ba3b77c30
ephysians <chuksobinna80@gmail.com>
2024-09-13 16:15:27 +0100

    latest update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,18 +20,7 @@ interface Repo {
   description?: string;
 }
 
-// const fetchUserData = async (username: string) => {
-//   const { data } = await axios.get(`https://api.github.com/users/${username}`);
-//   return data;
-  
-// };
 
-// const fetchUserRepos = async (username: string) => {
-//   const { data } = await axios.get(
-//     `https://api.github.com/users/${username}/repos`
-//   );
-//   return data;
-// };
 
 
 const fetchUserData = async (username: string) => {
@@ -111,137 +100,151 @@ const Index: NextPage = () => {
               </h1>
             </div>
 
-            <div className="flex w-full flex-col items-start justify-start gap-4">
-              <div className="flex w-full">
-                <input
-                  type="text"
-                  placeholder="Username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  className="w-full rounded-md border p-2 text-[#252525] hover:border-blue-300 outline-none"
-                />
-              </div>
-
-              {/* Radix Dialog for user profile display */}
-              <Dialog.Root>
-                <Dialog.Trigger asChild>
-                  <button
-                    onClick={handleSearch}
-                    className="absolute right-14 hover:bg-blue-300 rounded-r-md bg-[#6c757d] p-3 text-white"
-                  >
-                    <Search />
-                  </button>
-                </Dialog.Trigger>
-                <Dialog.Portal>
-                  <Dialog.Overlay
-                    className="fixed w-full inset-0 bg-[#252525] bg-opacity-90"
-                    onClick={(e) => e.stopPropagation()} // stops overlay from triggering events inside the modal
-                  />
-                  <Dialog.Content className="fixed inset-0 flex items-center justify-center p-6">
-                    <div className="relative w-full max-w-lg p-6 bg-white rounded-md shadow-lg top-7">
-                      <Dialog.Close asChild>
-                        <button
-                          className="absolute top-4 right-4 text-[#000] hover:text-[#f13939] p-2 rounded-full bg-slate-300"
-                          aria-label="Close"
-                        >
-                          <IoClose size={20} />
-                        </button>
-                      </Dialog.Close>
-
-                      {isUserLoading && (
-                        <div className="flex justify-center items-center">
-                          <FaSpinner
-                            className="animate-spin text-teal-500"
-                            size={24}
-                          />
-                        </div>
-                      )}
-
-                      {!isUserLoading && user && (
-                        <div className="flex flex-col items-center">
-                          <div className="mb-4">
-                            <Image
-                              src={user.avatar_url}
-                              alt={user.login}
-                              width={150} // Specify a width
-                              height={150} // Specify a height
-                              className="w-48 h-48 rounded-full"
-                            />
-
-                            <a
-                              href={user.html_url}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                              className="text-blue-500 mt-2 inline-block"
-                            >
-                              View Profile
-                            </a>
-                          </div>
-
-                          <div className="flex flex-col items-center">
-                            <h2 className="text-xl font-bold">{user.name}</h2>
-                            <p className="text-gray-600">{user.bio}</p>
-
-                            <div className="flex space-x-4 mt-2">
-                              <p className="font-semibold">
-                                Followers:{" "}
-                                <span className="text-blue-700">
-                                  {user.followers}
-                                </span>
-                              </p>
-                              <p className="font-semibold">
-                                Following:
-                                <span className="text-green-700">
-                                  {user.following}
-                                </span>
-                              </p>
-
-                              <p className="font-semibold">
-                                Repos:
-                                <span className="text-blue-700">
-                                  {user.public_repos}
-                                </span>
-                              </p>
-                            </div>
-                          </div>
-
-                          {repos && (
-                            <div className="w-full max-h-80 overflow-y-auto">
-                              {repos.slice(0, 5).map((repo: Repo) => (
-                                <div
-                                  key={repo.id}
-                                  className=" border border-blue-300 p-2 rounded-md mb-2"
-                                >
-                                  <a
-                                    href={repo.html_url}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="text-blue-500 text-sm"
-                                  >
-                                    {repo.name.toLocaleLowerCase()}
-                                  </a>
-                                </div>
-                              ))}
-                            </div>
-                          )}
-                        </div>
-                      )}
-
-                      {isUserError && (
-                        <p className="text-sm font-Exo text-[#f13939]">
-                          Error fetching user profile...try again
-                        </p>
-                      )}
-                      {isReposError && (
-                        <p className="text-sm font-Exo text-[#f13939]">
-                          Error fetching user repositories...try again
-                        </p>
-                      )}
-                    </div>
-                  </Dialog.Content>
-                </Dialog.Portal>
-              </Dialog.Root>
+            <div className="flex w-full">
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="w-full rounded-md border p-2 text-[#252525] hover:border-[#f77f00] outline-none"
+              />
             </div>
+          </div>
+
+          <div className="flex w-full flex-col items-start justify-start gap-4">
+            <Dialog.Root>
+              <Dialog.Trigger asChild>
+                <button
+                  onClick={handleSearch}
+                  className="absolute bottom-[82px] right-14 hover:bg-blue-300 rounded-r-md bg-[#6c757d] p-3 text-white"
+                >
+                  <Search />
+                </button>
+              </Dialog.Trigger>
+              <Dialog.Portal>
+                <Dialog.Overlay
+                  className="fixed w-full inset-0 bg-[#252525] bg-opacity-90"
+                  onClick={(e) => e.stopPropagation()} // stops overlay from triggering events inside the modal
+                />
+                <Dialog.Content className="fixed inset-0 flex items-center justify-center p-6">
+                  <div className="relative w-full max-w-lg p-6 bg-white rounded-md shadow-lg top-7">
+                    <Dialog.Close asChild>
+                      <button
+                        className="absolute top-4 right-4 text-[#000] hover:text-[#f13939] p-2 rounded-full bg-slate-300"
+                        aria-label="Close"
+                      >
+                        <IoClose size={20} />
+                      </button>
+                    </Dialog.Close>
+
+                    {isUserLoading && (
+                      <div className="flex justify-center items-center">
+                        <FaSpinner
+                          className="animate-spin text-teal-500"
+                          size={24}
+                        />
+                      </div>
+                    )}
+
+                    {!isUserLoading && user && (
+                      <div className="flex flex-col items-center">
+                        <div className="mb-4">
+                          <Image
+                            src={user.avatar_url}
+                            alt={user.login}
+                            width={150} // Specify a width
+                            height={150} // Specify a height
+                            className="w-48 h-48 rounded-full"
+                          />
+
+                          <a
+                            href={user.html_url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-blue-500 font-semibold text-sm font-Exo mt-6 flex justify-center items-center"
+                          >
+                            VIEW PROFILE
+                          </a>
+                          <div>
+                            <p className="font-semibold">
+                             Twitter:{" "}
+                              <span className="text-blue-700">
+                                {user.twitter_username}
+                              </span>
+                            </p>
+                          </div>
+                        </div>
+
+                        <div className="flex shadow-md border mb-4 px-2 bg-slate-100 rounded-md flex-col items-center">
+                          <h2 className="text-xl font-Exo font-bold">
+                            {user.name}
+                          </h2>
+                          <p className="text-black font-Exo text-sm flex justify-center items-center">
+                            {user.bio}
+                          </p>
+
+                          <div className="flex space-x-4 mt-2">
+                            <p className="font-semibold">
+                              Followers:{" "}
+                              <span className="text-blue-700">
+                                {user.followers}
+                              </span>
+                            </p>
+                            <p className="font-semibold">
+                              Following:{" "}
+                              <span className="text-green-700">
+                                {user.following}
+                              </span>
+                            </p>
+
+                            <p className="font-semibold ">
+                              Repos:{" "}
+                              <span className="text-blue-700 ">
+                                {user.public_repos}
+                              </span>
+                            </p>
+                          </div>
+                        </div>
+
+                        {repos && (
+                          <div className=" flex flex-col-reverse w-full h-64  overflow-y-auto">
+                            {repos.slice(0, 1000).map((repo: Repo) => (
+                              <div
+                                key={repo.id}
+                                className="border p-2 rounded-md mb-2"
+                              >
+                                <a
+                                  href={repo.html_url}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  className="text-blue-500 text-sm"
+                                >
+                                  {repo.name.toLocaleLowerCase()}
+                                </a>
+                              </div>
+                            ))}
+                          </div>
+                        )}
+                        <p className="font-Exo pt-4 text-[10px] text-amber-950">
+                          @Sprint-Technology....... developed by Chuks
+                        </p>
+                      </div>
+                    )}
+
+                    {isUserError && (
+                      <p className="text-sm font-Exo text-[#f13939]">
+                        Error fetching user profile...try again
+                      </p>
+                    )}
+                    {isReposError && (
+                      <p className="text-sm font-Exo text-[#f13939]">
+                        Error fetching user repositories...try again
+                      </p>
+                    )}
+                  </div>
+                </Dialog.Content>
+              </Dialog.Portal>
+            </Dialog.Root>
           </div>
         </div>
       </div>
